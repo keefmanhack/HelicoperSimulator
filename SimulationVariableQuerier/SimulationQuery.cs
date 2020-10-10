3960bafc435975674a642a98393966019a92b565
U-LAKEERIE\gregoire001 <gregoire001@Z344ECE10.gannon.edu>
2020-10-10 16:22:39 -0400

    Querier added

---
+++ SimulationVariableQuerier/SimulationQuery.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.FlightSimulator.SimConnect;
+using System.Runtime.InteropServices;
+
+
+namespace SimulationVariableQuerier
+{
+    class SimulationQuery
+    {
+        SimConnect sc = null;
+        public bool connectToSim()
+        {
+           const int WM_USER_SIMCONNECT = 0x0402;
+            try
+            {
+                sc = new SimConnect("Managed Data Request", this.Handle, WM_USER_SIMCONNECT, null, 0);
+                sc.OnRecvOpen += new SimConnect.RecvOpenEventHandler(SimConnect_OnRecvOpen);
+            }
+            catch(COMException ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+            return true;
+        }
+
+        private void SimConnect_OnRecvOpen(SimConnect sender, SIMCONNECT_RECV_OPEN data)
+        {
+
+        }
+
+    }
+
+}
